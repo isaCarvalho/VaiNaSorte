9550f567d6e797cc5152af786dd745f160c713dd
isaCarvalho <isabelacarvalhocf@hotmail.com>
2020-03-22 11:41:31 -0300

    Entrada e validacoes

--- app/src/main/java/com/example/vainasorte/MainActivity.kt
+++ app/src/main/java/com/example/vainasorte/MainActivity.kt
@@ -3,7 +3,9 @@ package com.example.vainasorte
 import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.viewpager.widget.ViewPager
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.tabs.TabLayout
@@ -47,8 +49,82 @@ class MainActivity : AppCompatActivity() {
 
         val fab = findViewById<FloatingActionButton>(R.id.floatingActionButton)
         fab.setOnClickListener {
-            findViewById<TextView>(R.id.numbersTextView).text = (1..100).random().toString()
+
+            // pega o limite infeiror na entrada
+            val limInfText = findViewById<EditText>(R.id.limInferior).text
+
+            // pega o limite superior na entrada
+            val limSupText = findViewById<EditText>(R.id.limSuperior).text
+
+            // valida o limite inferior
+            var inf = this.checkNullorEmpty(limInfText.toString(), "Limite inferior não preenchido. Valor padrão utilizado",
+                1)
+
+            inf = checkNegative(inf)
+
+            // valida o limite superior
+            var sup = checkNullorEmpty(limSupText.toString(), "Limite superior não preenchido. Valor padrão utilizado",
+                100)
+
+            sup = checkNegative(sup)
+
+            // se os limites forem iguais, utiliza os valores padrao
+            if (sup == inf)
+            {
+                Toast.makeText(applicationContext, "Limite superior igual ao limite inferior. Valores padrão serão utilizados",
+                    Toast.LENGTH_SHORT).show()
+
+                inf = 1
+                sup = 100
+            }
+
+            // se o limite superior for menor que o inferior, troca os valores
+            if (sup < inf)
+            {
+                Toast.makeText(applicationContext, "Limite superior menor que o limite inferior. Valores serão trocados",
+                    Toast.LENGTH_SHORT).show()
+
+                val aux = inf
+                inf = sup
+                sup = aux
+            }
+
+            // faz o sorteio
+            findViewById<TextView>(R.id.numbersTextView).text = (inf..sup).random().toString()
             findViewById<TextView>(R.id.letterTextView).text = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".random().toString()
         }
+    }
+
+    // verifica se o valor e negativo -- apenas para validacao
+    private fun checkNegative(value : Int) : Int
+    {
+        if (value < 0)
+            return value * (-1)
+
+        return value
+    }
+
+    // valida os valores de entrada
+    private fun checkNullorEmpty(text : String, message : String, defaultValue : Int) : Int
+    {
+        // converte para inteiro ou nulo
+        val value = text.toIntOrNull()
+
+        // se o texto for vazio
+        if (text.isEmpty())
+        {
+            // dispara a mensagem e retorna o valor padrão
+            Toast.makeText(applicationContext, message, Toast.LENGTH_SHORT).show()
+            return defaultValue
+        }
+        else
+        {
+            // se for nulo, retorna o valor padrão
+            if (value == null)
+                return defaultValue
+        }
+
+        // retorna o valor de entrada
+        return value
     }
 }
